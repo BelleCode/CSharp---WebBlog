a90fea44e499b657f9a5b1aad4063f659790f3ca
BelleCode <Belinda.tiberio@gmail.com>
2021-08-02 09:21:19 -0700

    Add project files.

---
+++ Models/Blog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSharp___WebBlog.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        // public string AuthorId {get;set} for more than one author
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Description (500 characters or less)")]
+        // Is it possible to include a countdown
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? Updated { get; set; } // short hand "public Nullable<DateTime> Updated { get; set; }"
+
+        // Navigational Properties - These properties allow us to move from one object to another related
+        public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+
+        //TIGHT COUPLING: public virtual List<Post> Posts { get; set; } = new List<Post>();
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharp___WebBlog.Models
+{
+    public class Post
+    {
+    }
+}
