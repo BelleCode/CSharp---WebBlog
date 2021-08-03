8468a8548e644039ce92f9d7023ae9961b6c5e2b
BelleCode <Belinda.tiberio@gmail.com>
2021-08-03 14:16:27 -0700

    Comment Model complete

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace CSharp___WebBlog.Models
@@ -7,24 +8,32 @@ namespace CSharp___WebBlog.Models
     {
         public int Id { get; set; }
 
-        // Foreign key
-        public int PostId { get; set; }
+        public int PostId { get; set; } // Foreign key
 
-        public string BlogUserId { get; set; }
+        public string AuthorId { get; set; }
+        public string ModeratorId { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "(500 characters or less)")]
+        public string Body { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime Created { get; set; }
 
         public DateTime? Updated { get; set; } //Nullable b/c someone may NOT have updated
+        public DateTime? Moderated { get; set; }
+        public DateTime? Deleted { get; set; }
 
-        [Required]
-        [Display(Name = "(500 characters or less)")]
         [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
-        public string Body { get; set; }
+        [Display(Name = "Moderated Comment")]
+        public string ModeratedBody { get; set; }
 
-        // Pav Props
-        public virtual Post Post { get; set; }
+        //Navigation Properties
+        public virtual Post Post { get; set; }        // Pav Props
 
+        public virtual IdentityUser Author { get; set; }
+        public virtual IdentityUser Moderator { get; set; }
         //public virtual BlogUser BlogUser { get; set; }
     }
 }
--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,11 @@ namespace CSharp___WebBlog.Models
     public class Tag
     {
         public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
         public string Text { get; set; }
 
         //Nav Prop
