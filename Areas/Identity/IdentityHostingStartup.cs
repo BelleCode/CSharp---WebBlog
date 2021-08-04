92d19eb54c5a5e0a46859399223569b2cd5e1ade
BelleCode <Belinda.tiberio@gmail.com>
2021-08-04 10:48:02 -0700

    Scaffolding all the identity pages

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CSharp___WebBlog.Data;
+using CSharp___WebBlog.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CSharp___WebBlog.Areas.Identity.IdentityHostingStartup))]
+namespace CSharp___WebBlog.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
